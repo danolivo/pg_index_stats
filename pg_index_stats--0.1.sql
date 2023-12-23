06bb4c932f11c703d807f06ebe4b22f772c71289
Andrey V. Lepikhov <a.lepikhov@postgrespro.ru>
2023-12-23 18:10:32 +0200

    Fix initialization script.

--- pg_index_stats--0.1.sql
+++ pg_index_stats--0.1.sql
@@ -1,5 +1,12 @@
+/* contrib/pg_index_stats/pg_index_stats--0.1.sql */
 
--- Pass through all the relations in the database and
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION pg_index_stats" to load this file. \quit
+
+--
+-- Manually create extended statistics using an index as a definition of
+-- expression
+--
 CREATE FUNCTION pg_catalog.build_extended_statistic(idxname text)
 RETURNS boolean
 AS 'MODULE_PATHNAME', 'build_extended_statistic'
