6866192261915a0170f21d403ac82b58bbf7fbbf
Andrey V. Lepikhov <a.lepikhov@postgrespro.ru>
2024-01-04 16:01:01 +0700

    Add into the extension interface the remove() and rebuild() routines to give user simple tools for cleanup and generation statistics over the indexes, created before the extension.

--- pg_index_stats--0.1.sql
+++ pg_index_stats--0.1.sql
@@ -12,3 +12,47 @@ CREATE FUNCTION pg_index_stats_build(idxname text,
 RETURNS boolean
 AS 'MODULE_PATHNAME', 'pg_index_stats_build'
 LANGUAGE C VOLATILE STRICT;
+
+--
+-- Remove all auto-generated statistics
+-- Return number of deleted statistics
+--
+CREATE FUNCTION pg_index_stats_remove() RETURNS integer AS $$
+WITH deleted AS (
+  DELETE FROM pg_statistic_ext s
+  WHERE
+    s.oid IN (SELECT d.objid FROM pg_depend d
+      JOIN pg_class c ON (d.refobjid = c.oid)
+	    JOIN pg_namespace n ON (c.relnamespace = n.oid)
+    WHERE
+      c.relkind = 'i' AND
+      n.nspname NOT IN ('pg_catalog', 'pg_toast', 'information_schema')
+    )
+  RETURNING *
+) SELECT count(*) FROM deleted;
+$$ LANGUAGE SQL PARALLEL SAFE STRICT;
+
+--
+-- Generate extended statistics on all non-system indexes
+-- Return number of rebuilt statistics
+--
+CREATE FUNCTION pg_index_stats_rebuild() RETURNS integer AS $$
+DECLARE
+ result integer;
+BEGIN
+  -- Pre-cleanup
+  PERFORM pg_index_stats_remove();
+
+  SELECT count(*) FROM (
+    SELECT pg_index_stats_build((c.oid::regclass)::text)
+	FROM pg_class c, pg_namespace n
+    WHERE
+      c.relkind = 'i' AND
+      c.relnamespace = n.oid AND
+      n.nspname NOT IN ('pg_catalog', 'pg_toast', 'information_schema')
+  ) AS q1
+  INTO result;
+
+  RETURN result;
+END;
+$$ LANGUAGE PLPGSQL PARALLEL SAFE STRICT;
